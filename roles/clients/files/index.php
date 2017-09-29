93e897bbf071031c8454e215f7718f8e4d2fab36
Martin van Es <martin@mrvanes.com>
2017-09-29 16:59:57 +0200

    Tiny fixes

--- roles/clients/files/index.php
+++ roles/clients/files/index.php
@@ -1,3 +1,8 @@
 <?php
-phpinfo();
+foreach ($_SERVER as $claim => $value) {
+    if (substr($claim, 0, 4) === 'OIDC') {
+        echo "<p>" . substr($claim, 5) . ": $value</p>\n";
+    }
+}
+//phpinfo();
 
