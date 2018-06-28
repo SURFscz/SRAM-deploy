df46d81228046c7ca390094556eb93a83be67e25
Martin van Es <martin@mrvanes.com>
2018-06-28 14:02:14 +0200

    ORCID IdP

--- roles/client-orcid-idp/files/orcid2urn.php
+++ roles/client-orcid-idp/files/orcid2urn.php
@@ -1,12 +1,8 @@
 <?php
 $attributemap = array(
     // Attributes returned by ORCID
-//     'orcid.uri'          => 'eduPersonOrcid', // URI with a 16-digit number
-                                              // compatible with ISO 27729,
-                                              // a.k.a. International Standard
-                                              // Name Identifier (ISNI)
-    'orcid.path'         => 'urn:mace:dir:attribute-def:uid', // The ORCID number formatted as
-                                              // xxxx-xxxx-xxxx-xxxx
+    'orcid.path'         => 'urn:mace:dir:attribute-def:uid',
+    'orcid.path'         => 'urn:mace:dir:attribute-def:eduPersonPrincipalName',
     'orcid.name'         => 'urn:mace:dir:attribute-def:displayName',
     'orcid.given-names'  => 'urn:mace:dir:attribute-def:givenName',
     'orcid.family-name'  => 'urn:mace:dir:attribute-def:sn',
