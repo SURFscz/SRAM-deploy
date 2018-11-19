ef5cd5318fe0f64dcd694f9d394b572a44890569
Bas Zoetekouw <bas.zoetekouw@surfnet.nl>
2018-11-19 11:29:55 +0100

    Fix callback plugin prototype

--- callback_plugins/summary_by_role.py
+++ callback_plugins/summary_by_role.py
@@ -28,7 +28,7 @@ class CallbackModule(CallbackBase):
     def v2_runner_on_ok(self, result):
         self._handle_result(result)
 
-    def v2_runner_on_failed(self, result):
+    def v2_runner_on_failed(self, result, ignore_errors=False):
         self._handle_result(result)
 
     def v2_runner_on_unreachable(self, result):
