382eb20dc34c90c90dda8f21998b4c3980b83a87
Martin van Es <martin@mrvanes.com>
2018-11-26 13:01:05 +0100

    Fix mysql table creation idempotency

--- roles/proxy_mysql/files/create_attributes_hash_db.sql
+++ roles/proxy_mysql/files/create_attributes_hash_db.sql
@@ -1,5 +1,14 @@
-CREATE TABLE `attributes_hash` (
-  `nameid` varchar(50) NOT NULL,
-  `hash` varchar(45) DEFAULT NULL,
-  UNIQUE KEY `nameid` (`nameid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
+DELIMITER $
+BEGIN NOT ATOMIC
+  IF ( select count(*) from information_schema.tables where TABLE_SCHEMA='attributes_hash' and TABLE_NAME='zone_people')=1
+  THEN
+    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT='table exists';
+  ELSE
+    CREATE TABLE `attributes_hash` (
+    `nameid` varchar(50) NOT NULL,
+    `hash` varchar(45) DEFAULT NULL,
+    UNIQUE KEY `nameid` (`nameid`)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
+  END IF;
+END $
+DELIMITER ;
