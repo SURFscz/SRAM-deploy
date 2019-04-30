0abf2dcfbf59f4ef57650ef7e439b2e89d7ba4f9
Bas Zoetekouw <bas.zoetekouw@surfnet.nl>
2019-04-30 14:35:13 +0200

    fix encoding issues in hash filter
    # Conflicts:
    #	filter_plugins/hashes.py

--- filter_plugins/hashes.py
+++ filter_plugins/hashes.py
@@ -14,8 +14,17 @@ class FilterModule(object):
         hash_b64 = base64.b64encode(hash_bin)
         return hash_b64.decode('ASCII')
 
+    # generate a MySQL-type password hash
+    # see https://unix.stackexchange.com/questions/44883/encrypt-a-password-the-same-way-mysql-does
+    @staticmethod
+    def _sha1_mysql(str):
+        str2 = str.encode('ASCII')
+        res = hashlib.sha1(hashlib.sha1(str).digest()).hexdigest()
+        return '*' + res.decode('ASCII').upper()
+
     def filters(self):
         return {
             'sha1_b64':   self._sha1_b64,
             'slapd_hash': lambda a: '{SHA}' + self._sha1_b64(a)
+            'mysql_hash': self._sha1_mysql
         }
