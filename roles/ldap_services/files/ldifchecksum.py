af5d7b413c4f67d6e2449af16bd84c1fd0c2a8f2
Martin van Es <martin@mrvanes.com>
2020-04-09 15:52:12 +0200

    Improve LDAP checksum calculation

---
+++ roles/ldap_services/files/ldifchecksum.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import sys
+import ldif
+import hashlib
+
+def freeze(o):
+    if isinstance(o, dict):
+        return frozenset({ k: freeze(v) for k, v in sorted(o.items())}.items())
+    if isinstance(o, list):
+        return tuple([freeze(v) for v in sorted(o)])
+    return o
+
+ldifparser = ldif.LDIFRecordList(sys.stdin)
+ldifparser.parse()
+data = { k: v for k, v in ldifparser.all_records }
+f = freeze(data)
+h = hashlib.sha256(str(f).encode('utf-8')).hexdigest()
+print(h)
