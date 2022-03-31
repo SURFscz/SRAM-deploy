038e252ca61e34d70ac06167cb92238b6cc73820
Bas Zoetekouw <bas.zoetekouw@surf.nl>
2022-03-31 15:39:38 +0200

    remove attick

--- attick/client-microsoft-idp/files/ms2urn.php
+++
@@ -1,9 +0,0 @@
-<?php
-$attributemap = array(
-    'sub' => 'urn:mace:dir:attribute-def:eduPersonPrincipalName',
-    'windowslive.id' => 'urn:mace:dir:attribute-def:uid',
-    'windowslive.displayName' => 'urn:mace:dir:attribute-def:cn',
-    'windowslive.givenName' => 'urn:mace:dir:attribute-def:givenName',
-    'windowslive.surname' => 'urn:mace:dir:attribute-def:sn',
-    'windowslive.userPrincipalName' => 'urn:mace:dir:attribute-def:mail',
-);
--- attick/oidc2samlidp/files/unspecified.py
+++
@@ -1,6 +0,0 @@
-MAP = {
-    'identifier': 'urn:oasis:names:tc:SAML:2.0:attrname-format:unspecified',
-    'fro': { },
-    'to': { }
-}
-
