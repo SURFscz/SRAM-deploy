90ae79fbafb5cc45685096e5fc6a2cd9c5a15e93
Bas Zoetekouw <bas.zoetekouw@surf.nl>
2023-08-21 16:50:30 +0200

    indents and quotes

--- roles/ci-test/files/resume.js
+++ roles/ci-test/files/resume.js
@@ -1,14 +1,14 @@
 fragment = window.location.hash.substring(1);
 //alert(fragment);
 if (fragment) {
-  params = fragment.split("&")
-  for (let i=0; i < params.length; i++) {
-    var p = params[i].split("=");
-    var input = document.createElement('input');
-    input.type = 'hidden';
-    input.name = p[0];
-    input.value = p[1];
-    document.forms['oidc'].appendChild(input);
-  }
-  document.forms['oidc'].submit();
+    params = fragment.split("&")
+    for (let i=0; i < params.length; i++) {
+        var p = params[i].split("=");
+        var input = document.createElement('input');
+        input.type = 'hidden';
+        input.name = p[0];
+        input.value = p[1];
+        document.forms['oidc'].appendChild(input);
+    }
+    document.forms['oidc'].submit();
 }
