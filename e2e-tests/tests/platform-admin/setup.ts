02ab1e03d30936e6080dd9978473e3ca40e87a96
Eric van Schaik <eric.vanschaik@surf.nl>
2025-12-22 20:12:49 +0100

    only test with firefox in ci, and take a shortcut for db seeding

--- e2e-tests/tests/platform-admin/setup.ts
+++ e2e-tests/tests/platform-admin/setup.ts
@@ -1,11 +1,7 @@
 import { Page } from "@playwright/test";
 
 export default async function seedDatabase(page: Page): Promise<void> {
-    await page.getByRole('button').filter({ hasText: /^$/ }).click();
-    await page.getByRole('link', { name: 'System' }).click();
-    await page.getByRole('button', { name: 'Seed' }).hover();
-    await page.screenshot({ path: 'with-highlight.png' });
-    await page.getByRole('button', { name: 'Seed' }).click();
+    await page.goto('https://sbs.scz-vm.net/system/seed');
     await page.getByRole('button', { name: 'Run' }).first().click();
     await page.getByRole('button', { name: 'Confirm' }).click();
     await page.getByText('I hereby certify that I have').click();
